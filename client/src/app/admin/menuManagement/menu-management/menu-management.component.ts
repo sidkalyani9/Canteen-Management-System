46546f837f553213062367d01514e89f88501a20
NamrataPanjwani <namratapanjwani1412@gmail.com>
2024-03-21 20:41:24 +0530

    Menu Management component created

---
+++ client/src/app/admin/menuManagement/menu-management/menu-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-management',
+  templateUrl: './menu-management.component.html',
+  styleUrl: './menu-management.component.css'
+})
+export class MenuManagementComponent {
+
+}
