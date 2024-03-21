d0882ad678d9826b782a789392fef85392c1edbc
sidkalyani9 <sidkalyani9@gmail.com>
2024-03-21 12:27:51 +0530

    Added admin folder, generated landing page component and adjusted route for the same

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './admin/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"admin",
+    component: LandingPageComponent,
+    children: [
+      // Admin's Child Routes
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
