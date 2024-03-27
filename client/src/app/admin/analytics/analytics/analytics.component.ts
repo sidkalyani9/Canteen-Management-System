96830222cb2d84ab079c6a53553153d87231067d
sidkalyani9 <sidkalyani9@gmail.com>
2024-03-27 12:27:37 +0530

    Created dynamic chart and adjusted analytic cards

--- client/src/app/admin/analytics/analytics/analytics.component.ts
+++ client/src/app/admin/analytics/analytics/analytics.component.ts
@@ -10,30 +10,51 @@ Chart.register(...registerables);
 })
 export class AnalyticsComponent implements OnInit {
   public chart: any;
+  public selectedArr=[];
 
   ngOnInit(): void {
+    this.selectedArr = this.dayArr;
+    this.createChart();
+    
+  }
+
+
+
+  dayArr: any =['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
+  '2022-05-14', '2022-05-15'];
+
+  momArr:any =["Jan", "Feb", "Mar", "Apr", "May", "Jun"];
+
+  monthOnMonth(){
+    Chart.getChart("chart1")?.destroy();
+    this.selectedArr = this.momArr
+    this.createChart();
+  }
+
+  dailyRevenue(){
+    Chart.getChart("chart1")?.destroy();
+    this.selectedArr = this.dayArr
     this.createChart();
   }
 
   createChart(){
   
-    this.chart = new Chart("MyChart", {
+    this.chart = new Chart("chart1", {
       type: 'bar', //this denotes tha type of chart
 
       data: {// values on X-Axis
-        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
-								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
+        labels: this.selectedArr, 
 	       datasets: [
           {
             label: "Sales",
             data: ['467','576', '572', '79', '92',
-								 '574', '573', '576'],
+								 '574'],
             backgroundColor: 'blue'
           },
           {
             label: "Profit",
             data: ['542', '542', '536', '327', '17',
-									 '0.00', '538', '541'],
+									 '0.00'],
             backgroundColor: 'limegreen'
           }  
         ]
