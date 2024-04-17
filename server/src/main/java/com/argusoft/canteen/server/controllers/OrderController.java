6c0af7b02a0512a7af488cced434b90b76a413b9
DhruvanVora <dhruvanvora1606@gmail.com>
2024-04-17 16:28:58 +0530

    added order backed methods

---
+++ server/src/main/java/com/argusoft/canteen/server/controllers/OrderController.java
@@ -0,0 +1,53 @@
+package com.argusoft.canteen.server.controllers;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import canteenmanagement.models.Order;
+import canteenmanagement.service.OrderService;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+	
+	@Autowired
+	private OrderService service;
+	
+	@GetMapping("/getAllOrders")
+	public List<Order> getAllOrders(){
+		return service.getAllOrders();
+	}
+	
+	@PostMapping("/addOrder")
+	public Order addOrder(@RequestBody Order order) {
+		order.setOrderUuid(UUID.randomUUID());
+//		order.setCreatedAt(new Date());
+		return service.addOrder(order);
+	}
+	
+	@DeleteMapping("/delete/{orderUuid}")
+	public void deleteOrder(@PathVariable UUID orderUuid) {
+		service.deleteOrder(orderUuid);
+	}
+	
+	@GetMapping("/getOrderById/{orderUuidId}")
+	public Optional<Order> findOrderById(@PathVariable UUID orderUuidId) {
+		return service.findOrderById(orderUuidId);
+		
+	}
+	
+	
+
+}
