9b236a7f18e3c0d782993d11351733383ec960a3
sidkalyani9 <sidkalyani9@gmail.com>
2024-04-18 15:40:26 +0530

    Created User Backend

---
+++ server/src/main/java/com/argusoft/canteen/server/controllers/UserController.java
@@ -0,0 +1,49 @@
+package com.argusoft.canteen.server.controllers;
+
+import com.argusoft.canteen.server.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.argusoft.canteen.server.model.canteenUser;
+
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/user")
+public class UserController {
+
+    public UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+    @PostMapping("/getUserByEmail")
+    public ResponseEntity<canteenUser> fetchUserByEmail(@RequestBody Map<String,Object> requestBody){
+        try{
+            String email = requestBody.get("email").toString();
+            canteenUser userDetails = userService.getUser(email);
+            return new ResponseEntity<>(userDetails, HttpStatus.OK);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<canteenUser> saveUser(@RequestBody canteenUser user){
+        try{
+            canteenUser userDetails = userService.insertUser(user);
+            return new ResponseEntity<>(userDetails, HttpStatus.OK);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+}
