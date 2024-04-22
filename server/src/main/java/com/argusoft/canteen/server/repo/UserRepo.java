73cc78c6435bcaeb85681cedbb56d75b975f779a
DhruvanVora <dhruvanvora1606@gmail.com>
2024-04-22 15:29:20 +0530

    added addAmountToUser and addAmountToAllUser methods in walletHistory

--- server/src/main/java/com/argusoft/canteen/server/repo/UserRepo.java
+++ server/src/main/java/com/argusoft/canteen/server/repo/UserRepo.java
@@ -3,8 +3,11 @@ package com.argusoft.canteen.server.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.argusoft.canteen.server.model.canteenUser;
 
+import java.util.Optional;
 import java.util.UUID;
+
 
 public interface UserRepo extends JpaRepository<canteenUser, UUID> {
     canteenUser findByEmail(String Email);
+    Optional<canteenUser> findByEmployeeId(String employeeId);
 }
