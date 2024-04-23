3718bd4cff78ff84bff66985edaf9a2aea097a47
Gurkirat Singh <ghans0017@gmail.com>
2024-04-23 09:19:11 +0530

    coupon

--- client/src/app/admin/Coupon-Management/coupon/coupon.component.ts
+++ client/src/app/admin/Coupon-Management/coupon/coupon.component.ts
@@ -10,13 +10,15 @@ import { NgForm } from '@angular/forms';
 })
 export class CouponComponent implements OnInit {
   public coupons: Coupons[] = [];
+  public editCouponvalue: Coupons;
   constructor(private couponService: CouponService) { }
-  ngOnInit(): void { this.getallCoupons(); }
+  ngOnInit(): void { this.getallCoupons(); 
+    this.editCouponvalue={couponName: "",  description: "",activeTill:new Date(),discountType: "",deductionAmount:0,minValue:0};
+  }
   //get all coupons
   public getallCoupons(): void {
     this.couponService.getallCoupons().subscribe({
       next: (response: Coupons[]) => { this.coupons = response, console.log(this.coupons) },
-
       error: (error) => console.log('Error'),
       complete: () => console.log("complete")
     });
@@ -24,15 +26,18 @@ export class CouponComponent implements OnInit {
 
   // ---------------------------------------------------------------------------------------
   // edit modal functions
-  editModal(): void {
+  editModal(coupon : Coupons): void { 
+   
+    this.editCouponvalue=coupon;  
+    console.log("edit coupon");
+    console.log(this.editCouponvalue);
     const modal = document.getElementById('editCouponModal');
     if (modal != null) {
-      modal.style.display = 'block';
+      modal.style.display = 'block'; 
     } else {
       alert('Please fill the coupon details correctly!');
       console.log("Error in displaying Edit Modal");
     }
-
   }
   closeeditmodal() {
     const modal = document.getElementById('editCouponModal');
@@ -57,6 +62,7 @@ export class CouponComponent implements OnInit {
     } else {
       alert('Please fill the coupon details first!');
     }
+ 
   }
   // -------------------------------------------------------------------------------------
   //delete modal functions
@@ -80,13 +86,19 @@ export class CouponComponent implements OnInit {
     this.couponService.addnewCoupon(couponform.value).subscribe(() => {
       console.log(couponform.value);
       this.getallCoupons();
+      this.closeaddmodal();
+      couponform.resetForm();
     });
   }
   deleteCoupon() {
 
   }
-  editcoupon(arg0: any) {
-
+  editcoupon(coupon : NgForm):void {
+    this.couponService.updateCoupon(coupon.value).subscribe((data: any) =>{     
+      console.log("Update Successful");
+     this.getallCoupons();
+      this.closeeditmodal();
+    });
   }
 
 
