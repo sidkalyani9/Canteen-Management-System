5e2e723cb115b1adb87a3063d30a6f73a136e393
Gurkirat Singh <122076512+ghans17@users.noreply.github.com>
2024-04-29 15:07:20 +0530

    Delete client/src/app/admin/Coupon-Management/Coupons.DataService.ts

--- client/src/app/admin/Coupon-Management/Coupons.DataService.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Coupons } from './coupon/Coupons';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class DataService {
-    public coupons:Coupons[]=[];
-}
